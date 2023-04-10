9c33154e5abe487befb999d86ed5910c5e2d8527
Nneif <nneifemakpunonu@gmail.com>
2023-04-10 15:15:14 -0400

    stored 2 questions into an array and testing the button to see if it works

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-var startButton = document.querySelector("#start");
+var changeQuestions = document.querySelector("#quiz-overview");
 
-startButton.addEventListener("click", function(){
-    console.log("click me"); 
+var questionsWithChoices = [
+    {
+        question: "How are arrays determined?",
+        answer1: "Parenthesis ()",
+        answer2: "Curly Bracket {}",
+        answer3: "Bracket []",
+        answer4: "None of the above"
+    },
+
+    {
+        question: "What defines a class and what defines a id",
+        answer1: "class: # , id: .",
+        answer2: "class: . , id: +",
+        answer3: "class () , id: #",
+        answer4: "class: . , id: #"
+    }
+]
+
+changeQuestions.addEventListener("click", function(){
+    console.log("click me");
 })
