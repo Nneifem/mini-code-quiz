b944b5a3b81fd1ff809742e85176dbc9fdbf1602
Nneif <nneifemakpunonu@gmail.com>
2023-04-11 12:30:26 -0400

    make question 1 appear on screen with avaliable answers

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-var changeQuestions = document.querySelector("#quiz-overview");
+var landingPage = document.querySelector(".quiz-overview");
+var questionContainer = document.querySelector("#question-container");
+var questionTextEl = document.querySelector("#js-question-text");
+var answersList = document.querySelector("#js-question-choices");
+var startButton = document.querySelector("#start");
 
 var questionsWithChoices = [
     {
         question: "How are arrays determined?",
-        answer1: "Parenthesis ()",
-        answer2: "Curly Bracket {}",
-        answer3: "Bracket []",
-        answer4: "None of the above"
+        answers: ["Parenthesis ()", "Curly Bracket {}", "Bracket []", "None of the above"],
+        correctAnswer: "Bracket []"
     },
 
     {
         question: "What defines a class and what defines a id",
-        answer1: "class: # , id: .",
-        answer2: "class: . , id: +",
-        answer3: "class () , id: #",
-        answer4: "class: . , id: #"
+        answers: ["class: # , id: .", "class: . , id: +", "class () , id: #", "class: . , id: #"],
+        correctAnswer: "class: . , id: #"
     }
 ]
+var currentQuestionIndex = 0;
 
-changeQuestions.addEventListener("click", function(){
-    console.log("click me");
+startButton.addEventListener("click", function(){
+    // TODO: seperate lines 24-33 into a function
+
+    questionTextEl.textContent = questionsWithChoices[currentQuestionIndex].question;
+    for (var i = 0; i < questionsWithChoices[currentQuestionIndex].answers.length; i++){
+        var currentAnswer = questionsWithChoices[currentQuestionIndex].answers[i];
+        var newButton =  document.createElement("button");
+        newButton.textContent = currentAnswer;
+
+        // TODO: add click event listener to newButton that checks if answer is correct
+
+        answersList.append(newButton)
+    }
+
+    landingPage.classList.add("hide");
 })
