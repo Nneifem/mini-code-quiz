d4ec7d693f94572f79138166181d774984efbc59
Nneif <nneifemakpunonu@gmail.com>
2023-04-11 22:53:24 -0400

    added code to indicate if the user is clicking on right button and working on looping through the next question

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var questionContainer = document.querySelector("#question-container");
 var questionTextEl = document.querySelector("#js-question-text");
 var answersList = document.querySelector("#js-question-choices");
 var startButton = document.querySelector("#start");
+var rightAnswerEl = document.querySelector("#right");
+var wrongAnswerEl = document.querySelector("#wrong");
 
 var questionsWithChoices = [
     {
@@ -18,20 +20,64 @@ var questionsWithChoices = [
     }
 ]
 var currentQuestionIndex = 0;
+var theRightAnswer = questionsWithChoices[currentQuestionIndex].correctAnswer;
+
 
 startButton.addEventListener("click", function(){
-    // TODO: seperate lines 24-33 into a function
-
+    function theQuestions(){  
     questionTextEl.textContent = questionsWithChoices[currentQuestionIndex].question;
     for (var i = 0; i < questionsWithChoices[currentQuestionIndex].answers.length; i++){
         var currentAnswer = questionsWithChoices[currentQuestionIndex].answers[i];
         var newButton =  document.createElement("button");
+        newButton.setAttribute("id", "button" + i)
         newButton.textContent = currentAnswer;
-
-        // TODO: add click event listener to newButton that checks if answer is correct
 
         answersList.append(newButton)
     }
+}
+    var buttonOne = document.querySelector("#button0");
+    buttonOne.addEventListener("click", function(){
+        console.log(buttonOne.textContent);
+        checkAnswer(questionsWithChoices[currentQuestionIndex], buttonOne.textContent)
+    })
 
+    var buttonTwo = document.querySelector("#button1");
+    buttonTwo.addEventListener("click", function(){
+        console.log(buttonTwo.textContent);
+        checkAnswer(questionsWithChoices[currentQuestionIndex], buttonTwo.textContent)
+    
+    })
     landingPage.classList.add("hide");
+
+    var buttonThree = document.querySelector("#button2");
+    buttonThree.addEventListener("click", function(){
+        console.log(buttonThree.textContent);
+        checkAnswer(questionsWithChoices[currentQuestionIndex], buttonThree.textContent)
+    
+    })
+
+    var buttonFour = document.querySelector("#button3");
+    buttonFour.addEventListener("click", function(){
+        console.log(buttonFour.textContent);
+        checkAnswer(questionsWithChoices[currentQuestionIndex], buttonFour.textContent)
+    })
 })
+
+function checkAnswer(theRightAnswer,userAnswer){
+    if(userAnswer == theRightAnswer.correctAnswer){
+        console.log("right")
+    }
+    else{
+        console.log("wrong")
+    }
+}
+currentQuestionIndex++;
+checkAnswer(theRightAnswer,userAnswer);
+// var buttonOne = document.querySelector("#0");
+// buttonOne.addEventListener("click", function(){
+//     var theRightAnswer = questionsWithChoices[currentQuestionIndex].correctAnswer;
+     
+//     console.log(theRightAnswer);
+//     console.log(buttonOne.textContent);
+
+// })
