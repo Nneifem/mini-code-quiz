4640381d971032991648adc9390f1b00decb4ba4
Nneif <nneifemakpunonu@gmail.com>
2023-04-13 16:44:23 -0400

    fixed some code in the html and added some query selectors in the new js file

---
+++ scriptcopy.js
@@ -0,0 +1,6 @@
+var titlePage = document.querySelector(".quiz-overview");
+var timerEl = document.querySelector(".timer");
+var questionContainer = document.querySelector("#question-container");
+var questionsEl = document.querySelector("#question-text");
+var answersEl = document.querySelector("question-choices");
+var finalScore = document.querySelector("#score-display");
